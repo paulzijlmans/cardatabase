71dfbf1fb1dc53bd8deb22732e5904b604ace612
Paul Zijlmans <paulzijlmans@hotmail.com>
2026-01-05 17:06:23 +0100

    Set Up Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { AppBar, Container, CssBaseline, Toolbar, Typography } from '@mui/material'
+
+function App() {
+  return (
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Car Shop</Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  )
+}
+
+export default App
